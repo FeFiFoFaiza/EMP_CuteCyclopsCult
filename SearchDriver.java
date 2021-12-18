2c73d3bf72bca052f0ab9f25d148b1b6bf6f5ff8
Rin Fukuoka <RIN@rinsmacbook957.lan>
2021-12-18 17:26:12 -0500

    added compareChange

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,6 +1,5 @@
 public class SearchDriver {
 
-
   //populate array
   public static void populate(Comparable[] a){
     for (int i = 0; i < a.length; i++){
@@ -8,6 +7,7 @@ public class SearchDriver {
     }
 
   }
+
 
   //record time for Binary Search
   public static long binTime(Comparable[] a, int target){
@@ -36,23 +36,66 @@ public class SearchDriver {
     return diff;
   }
 
+/*
   //compare the two Search methods
   public static void compare(Comparable[] arr, int target){
     long linT, binT, timeDiff;
     linT = linTime(arr, target);
     binT = binTime(arr, target);
+    timeDiff = Math.abs(binT - linT);
+
     System.out.println("linT: " + linT);
     System.out.println("binT: " + binT);
     System.out.println("Testing with dataset size: " + arr.length);
 
     if (linT < binT) {
-      timeDiff = binT - linT;
       System.out.println("LinearSearch is faster by " +
                          timeDiff + " milliseconds.");
     } else if (linT == binT) {
       System.out.println("Took the same time.");
     } else if (linT > binT) {
-      timeDiff = linT - binT;
+      System.out.println("BinarySearch is faster by " +
+                         timeDiff + " milliseconds.");
+    }
+
+  }
+*/
+
+  //compare the two Search methods with multiple trials
+  public static void compareAvg(Comparable[] arr){
+    int trials = 100; //set to 100 trials now
+    int target;
+    long linAvg, binAvg, linSum, binSum;
+    long timeDiff;
+    linSum = 0;
+    binSum = 0;
+
+    System.out.println("Testing with dataset size: " + arr.length);
+
+    //take avg of linearSearch trials
+    for (int i = 0; i < trials; i++){
+      target = (int)(Math.random()*arr.length);
+      linSum += linTime(arr, target);
+    }
+    linAvg = linSum / trials;
+
+    //take avg of binarySeach trials
+    for (int i = 0; i < trials; i++){
+      target = (int)(Math.random()*arr.length);
+      binSum += binTime(arr, target);
+    }
+    binAvg = binSum / trials;
+
+
+
+    timeDiff = Math.abs(linAvg - binAvg);
+    //compare averages
+    if (linAvg < binAvg) {
+      System.out.println("LinearSearch is faster by " +
+                         timeDiff + " milliseconds.");
+    } else if (linAvg == binAvg) {
+      System.out.println("LinearSearch and BinarySearch took the same time.");
+    } else if (linAvg > binAvg) {
       System.out.println("BinarySearch is faster by " +
                          timeDiff + " milliseconds.");
     }
@@ -60,26 +103,20 @@ public class SearchDriver {
   }
 
 
-
-  //store multiple test cases?
-  /*
-  public static linResults( long a);
-
-  public static binResults( long a);
-  */
-
-
   public static void main(String[] args){
-    long beforeS, afterS;
 
-    //test case with 10000000
-    Comparable[] test1 = new Comparable[10000000];
+    //test case with 0.75 million numbers
+    Comparable[] test1 = new Comparable[750_000];
     populate(test1);
+    compareAvg(test1);
 
-    compare(test1, 99999);
-    compare(test1, -6);
+    System.out.println();
+    System.out.println();
 
-
+    //test case with 100 million numbers
+    Comparable[] test2 = new Comparable[100_000_000];
+    populate(test2);
+    compareAvg(test2);
 
   } //end of main
 } //end of class
